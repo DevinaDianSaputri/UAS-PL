8ea5019eb8126f28ee9f465911326d295a9f7370
Devina Dian <10241022@student.itk.ac.id>
2025-11-28 08:00:08 +0800

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,39 +1,38 @@
 -- Buat database
 CREATE DATABASE IF NOT EXISTS sistem_cuti;
 USE sistem_cuti;
-
 -- Tabel departemen
 CREATE TABLE departemen (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id_departemen INT PRIMARY KEY AUTO_INCREMENT,
     nama_departemen VARCHAR(100) NOT NULL
 );
 
 -- Tabel users
 CREATE TABLE users (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id_user INT PRIMARY KEY AUTO_INCREMENT,
     nip VARCHAR(20) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
     nama_lengkap VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL,
     no_telp VARCHAR(15) NOT NULL,
     role ENUM('admin', 'pegawai') NOT NULL,
-    departemen_id INT NULL,
-    FOREIGN KEY (departemen_id) REFERENCES departemen(id)
+    id_departemen INT NULL,
+    FOREIGN KEY (id_departemen) REFERENCES departemen(id_departemen)
 );
 
 -- Tabel Jenis Cuti
 CREATE TABLE jenis_cuti (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id_jenis INT PRIMARY KEY AUTO_INCREMENT,
     nama_jenis VARCHAR(100) NOT NULL,
-    deskripsi TEXT,
-    maksimal_hari INT NOT NULL DEFAULT 0
+    deskripsi_cuti TEXT,
+    maks_hari INT NOT NULL DEFAULT 0
 );
 
 -- Tabel pengajuan_cuti
 CREATE TABLE pengajuan_cuti (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    user_id INT NOT NULL,
-    jenis_cuti_id INT NOT NULL,
+    id_pengajuan INT PRIMARY KEY AUTO_INCREMENT,
+    id_user INT NOT NULL,
+    id_jenis INT NOT NULL,
     tanggal_mulai DATE NOT NULL,
     tanggal_selesai DATE NOT NULL,
     alasan TEXT NOT NULL,
@@ -42,8 +41,8 @@ CREATE TABLE pengajuan_cuti (
     alasan_admin TEXT NULL,
     tanggal_disetujui TIMESTAMP NULL,
     dibuat_pada TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES users(id),
-    FOREIGN KEY (jenis_cuti_id) REFERENCES jenis_cuti(id)
+    FOREIGN KEY (id_user) REFERENCES users(id_user),
+    FOREIGN KEY (id_jenis) REFERENCES jenis_cuti(id_jenis)
 );
 
 INSERT INTO departemen (nama_departemen) VALUES 
